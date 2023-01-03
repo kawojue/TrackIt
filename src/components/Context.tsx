2ae8868322dbb6a6905a4d5d7f0bfbf68dba6679
Raheem Kawojue <kawojue08@gmail.com>
2023-01-03 21:18:41 +0100

    [fetch] user data

--- src/components/Context.tsx
+++ src/components/Context.tsx
@@ -6,17 +6,49 @@ const Context: any = createContext({})
 export const DataProvider:React.FC<{children: React.ReactElement}> = ({ children }) => {
     const [error, setError] = useState<string | null>(null)
     const [coords, setCoords] = useState<ICoords | null>(null)
+    const [userIP, setUserIP] = useState<{ip: string}>({ip: ""})
+    const [userInfo, setUserInfo] = useState<any>({})
+
+    let ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g
+
+    const getUserIP = async ():Promise<void> => {
+        try {
+            const res = await fetch("https://api.ipify.org?format=json")
+            const ip = await res.json()
+            setUserIP(ip)
+        } catch {
+            setError("IP not found!")
+        }
+    }
+
+    const getUserInfo = async ():Promise<void> => {
+        try {
+            const res = await fetch(`http://api.ipstack.com/102.88.35.55?access_key=3fa6a454f519adfa0d7e1ed5fb873e85`)
+            const data = await res.json()
+            setUserInfo(data)
+        } catch {
+
+        }
+    }
+
+    console.log(userInfo)
+
+    
 
     function showError(error:any):void {
         switch(error.code) {
         case error.PERMISSION_DENIED:
             setError("User denied the request for Geolocation.")
+            break
         case error.POSITION_UNAVAILABLE:
             setError("Location information is unavailable.")
+            break
         case error.TIMEOUT:
             setError("The request to get user location timed out.")
+            break
         case error.UNKNOWN_ERROR:
             setError("An unknown error occurred.")
+            break
         }
     }
 
@@ -34,19 +66,23 @@ export const DataProvider:React.FC<{children: React.ReactElement}> = ({ children
         })
     }
 
+    console.log(userIP)
+
     useEffect(() => {
+        (async () => await getUserInfo())();
+        (async () => await getUserIP())()
         setTimeout(() => {
             getGeolocation()
         }, 1500)
         return () => navigator.geolocation.clearWatch(0)
-    })
+    }, [])
     
     
 
 
     return (
         <Context.Provider value={{
-            coords, error
+            coords, error, userInfo, userIP
         }}>
             {children}
         </Context.Provider>
