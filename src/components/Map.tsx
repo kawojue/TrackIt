5a7c6d912cd9a2714621391c5609d89e90da743b
Raheem Kawojue <kawojue08@gmail.com>
2023-01-05 01:42:18 +0100

    [rem] userIP

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,7 +2,15 @@ import Context from "./Context"
 import { useContext } from 'react'
 
 const Map:React.FC = () => {
-    const {coords, error, userInfo, userIP}  = useContext<any>(Context)
+    const {coords, error, userInfo, isLoading}  = useContext<any>(Context)
+
+    if (isLoading) {
+        return (
+            <>
+                Loading...
+            </>
+        )
+    }
 
     if (error) {
         return (
@@ -13,27 +21,20 @@ const Map:React.FC = () => {
     }
     
     return (
-        <>
-        {coords ?
-            <section>
-                <p>
-                    Longitude: {coords.longitude}
-                </p>
-                <p>
-                    Latitude: {coords.latitude}
-                </p>
-                <p>
-                    Accuracy: less than {coords.accuracy.toFixed(3)} metres.
-                </p>
-                <p>
-                    IP: {JSON.stringify(userIP)}
-                </p>
-                <p>
-                    User Info: {JSON.stringify(userInfo)}
-                </p>
-            </section>
-        : <p>Loading...</p>}
-        </>
+        <section>
+            <p>
+                Longitude: {coords.longitude}
+            </p>
+            <p>
+                Latitude: {coords.latitude}
+            </p>
+            <p>
+                Accuracy: less than {coords.accuracy.toFixed(3)} metres.
+            </p>
+            <p>
+                User Info: {JSON.stringify(userInfo)}
+            </p>
+        </section>
     )
 }
 
